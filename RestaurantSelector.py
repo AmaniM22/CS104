09257d15500fdc0b2246d4064ded9c7635e1b77a
AmaniM22 <91150774+AmaniM22@users.noreply.github.com>
2022-02-07 12:56:49 -0500

    Restaurant Selection

---
+++ RestaurantSelector.py
@@ -0,0 +1,33 @@
+#Amani Minaya
+#Restaurant selector lab assignment
+vegan=False
+veggie=False 
+glut=False
+
+#recieving users input
+response=str(input('Is anyone in your party a vegetarian?: '))
+if response=='yes':
+    veggie=True
+response=str(input('Is anyone in your party vegan?: '))
+if response=='yes':
+    vegan= True
+response=str(input('is anyone in your party glutan free?: '))
+if response=='yes':
+    glut=True
+
+#displaying the restaurant options   
+print('Here are your restrant options: ')
+if veggie==False and vegan==False and glut==False:
+    print("Joe's Gourmet Burgers")
+    print("Mama's Fine Italian")
+    print("Main street Pizza")
+if veggie==True and vegan==False and glut==False:
+    print("Mama's Fine Italian")
+    print("Main street Pizza")
+if veggie==True and vegan==False and glut==True:
+    print("Main street Pizza")
+else:
+    print("Corner Cafe")
+    print("Chef's Kitchen")
+
+
