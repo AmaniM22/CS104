e607fd9a9a30af66b6d73aa9fbd44ffff123e57c
AmaniM22 <91150774+AmaniM22@users.noreply.github.com>
2022-02-18 16:55:54 -0500

    Update RestaurantSelector.py

--- RestaurantSelector.py
+++ RestaurantSelector.py
@@ -32,4 +32,37 @@ else:
     print("Corner Cafe")
     print("Chef's Kitchen")
 
+keep_going=str(input('Would you like to run it again?: '))
+while keep_going=='yes':
+    #recieving users input
+    response=str(input('Is anyone in your party a vegetarian?: '))
+    if response=='yes':
+        veggie=True
+    response=str(input('Is anyone in your party vegan?: '))
+    if response=='yes':
+        vegan= True
+    response=str(input('is anyone in your party glutan free?: '))
+    if response=='yes':
+        glut=True
+
+    #displaying the restaurant options   
+    print('Here are your restrant options: ')
+    if veggie==False and vegan==False and glut==False:
+        print("Joe's Gourmet Burgers")
+        print("Mama's Fine Italian")
+        print("Main street Pizza")
+    if veggie==True and vegan==False and glut==False:
+        print("Mama's Fine Italian")
+        print("Main street Pizza")
+    if veggie==True and vegan==False and glut==True:
+        print("Main street Pizza")
+        print("Corner Cafe")
+        print("Chef's Kitchen")
+    else:
+        print("Corner Cafe")
+        print("Chef's Kitchen")
+    keep_going=str(input('Would you like to run it again?: '))
+if keep_going=='no':
+    print('ok hope you enjoy ur meal!')   
+   
 
